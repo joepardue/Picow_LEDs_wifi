dc0f57c63e135a99c9d7c32be9fd0aebbd89b241
Joe Pardue <joepardue47@gmail.com>
2024-12-06 17:13:26 -0500

    Create 120624 secrets.py
    This must have an actual SSID and PASSWORD entered in the quoted to access your particular network and it must be in you picow CIRCUITPY directory.

---
+++ 120624 secrets.py
@@ -0,0 +1,6 @@
+# this must have an actual SSID and PASSWORD entered in the quoted to access your particular network
+# and it must be in you picow CIRCUITPY directory.
+secrets = {
+    "ssid": "Your_SSID",
+    "password": "Your_PASSWORD",
+}
